af81579734733137ab556be7facd24f215b75f21
Denisa Dupu <dupudenisa@gmail.com>
2020-04-05 15:26:13 -0500

    Source code to master

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        This is my About Page
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+function Contact(props) {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        This is my Contact Page
+      </p>
+    
+    </div>
+  );
+}
+
+export default Contact;
