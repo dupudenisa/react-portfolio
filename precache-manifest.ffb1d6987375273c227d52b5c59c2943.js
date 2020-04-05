14159ec371260d15e5e5d6f009b8fb636ab797f2
Denisa Dupu <dupudenisa@gmail.com>
2020-04-05 15:21:00 -0500

    Updates

---
+++ precache-manifest.ffb1d6987375273c227d52b5c59c2943.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b47b77bc4823c46aabbc7cc6d3e7da0",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d7b56f68c23ebb71b3a5",
+    "url": "/react-portfolio/static/js/2.61ed05d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.61ed05d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1df0ea4a4f0ce8607b",
+    "url": "/react-portfolio/static/js/main.99fbba9d.chunk.js"
+  },
+  {
+    "revision": "eff633beff9fe05cfab3",
+    "url": "/react-portfolio/static/js/runtime-main.6b1a3ce1.js"
+  }
+]);
